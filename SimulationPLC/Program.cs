df24554c7731bffbab6f96cef59fd71752a5d37e
Rodion Kotov <r.kotov@g.nsu.ru>
2023-12-14 10:32:38 +0700

    #2 Debug: - работы конвернетера float

--- SimulationPLC/Program.cs
+++ SimulationPLC/Program.cs
@@ -16,21 +16,41 @@ class Program
             var factory = new ModbusFactory();
             var master = factory.CreateTcpMaster(tcpClient);
 
-            // Example: Reading holding registers
-            ushort startAddress = 0xA01;
-            ushort numberOfPoints = 1;
+            // Example: Write a float value to a single register
+            float floatValue = 123.45f;
+            ushort registerAddress = 0xC04; // Replace with your desired register address
+
+            // Convert float to two 16-bit integers (words)
+            ushort[] values = BitConverter.GetBytes(floatValue).ToUShortArray();
 
             try
             {
-                await master.WriteSingleRegisterAsync(1, startAddress, 100);
-                
-                Console.WriteLine("Read Holding Registers:");
-               
+                await master.WriteMultipleRegistersAsync(1, registerAddress, values);
+                Console.WriteLine($"Float value {floatValue} written to register {registerAddress}.");
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"Error: {ex.Message}");
             }
         }
+    }
+}
+
+public static class BitConverterExtensions
+{
+    public static ushort[] ToUShortArray(this byte[] bytes)
+    {
+        if (bytes.Length % 2 != 0)
+            throw new ArgumentException("Byte array length must be an even number.");
+
+        ushort[] shorts = new ushort[bytes.Length / 2];
+
+        for (int i = 0, j = 0; i < bytes.Length; i += 2, j++)
+        {
+            shorts[j] = BitConverter.ToUInt16(bytes, i);
+        }
+        
+        Array.Reverse(shorts);
+        return shorts;
     }
 }
